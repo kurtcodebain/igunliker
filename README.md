fcc0b6e41055059a8a2faaf7ec15b983f09d2b34
Mikkel Holmgaard <mikkel.holmgaard@lego.com>
2024-03-27 20:51:08 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# igunlike
+# IG-unliker/iGunLiker/i🔫liker 
+
+Instagram unliking script!
+
+## Prerequisites
+
+2FA must be disabled!
+
+## How To Run
+
+Replace `???` placeholders in `igun.ts`
+
+`npm i`
+
+`npx ts-node igun.ts`
+
+## Info
+
+Instagram (API) limits the amount of commands (unlikes in this case) you can do within a certain timeframe. 
+
+Usually you only need to reauthorize your account via the Instagram app or webpage to get going again. 
+
+Sometimes you are being timed out and have to wait for up to 10 minutes before being able to rerun the script again.
---
+++ igun.ts
@@ -0,0 +1,56 @@
+import { IgApiClient, LikedFeedResponseRootObject } from 'instagram-private-api';
+import crypto from 'crypto';
+
+const USERNAME = "???";
+const PASSWORD = "???";
+
+const ig = new IgApiClient();
+
+const fetchLikedItems = async (): Promise<LikedFeedResponseRootObject> => {
+    console.log("Fetching liked items..")
+    const res = await ig.feed.liked().request();
+    console.log("OK");
+
+    return res;
+};
+
+const run = async () => {
+    ig.state.generateDevice(crypto.randomUUID());
+
+    let res = {} as LikedFeedResponseRootObject;
+    let loggedIn = false;
+
+    try {
+        console.log("Trying to log in..")
+        await ig.account.login(USERNAME, PASSWORD);
+        loggedIn = true;
+        console.log("OK")
+
+        res = await fetchLikedItems();
+
+        while (res.more_available) {
+            for (const item of res.items) {
+                await ig.media.unlike({ mediaId: item.id, moduleInfo: { module_name: 'feed_contextual_newsfeed_multi_media_liked' }});
+                console.log("Unliked " + item.id);
+            }
+    
+            res = await fetchLikedItems();
+        }
+    } catch (error: any) {
+        if (error.message.includes("400 Bad Request; Please wait a few minutes before you try again.")) {
+            console.log("Try again in a few minutes");
+        }
+        if (error.message.includes("400 Bad Request; challenge_required")) {
+            console.log("Manual reauthorization in app or web required")
+        }
+    } finally {
+        if (loggedIn) {
+            console.log("Logging out and destroying session..");
+            await ig.account.logout();
+        }
+
+        ig.destroy();
+    } 
+}
+
+void run();
